7ebf55690eb1d6bedf095ffcef95750bdf2bd351
naoisobe <ganmodoki7.43dari@gmail.com>
2023-01-15 14:27:06 +0900

    renewal

---
+++ js/script.js
@@ -0,0 +1,30 @@
+//ファーストビューのslider
+$('.slider').slick({
+  autoplay: true,
+  autoplaySpeed: 2000,
+  speed: 1000,
+  fade: true,
+  cssEase: 'linear'
+});
+
+
+//ハンバーガーメニューの開閉
+$('.burger-btn').on('click',function(){
+  $('.header-nav').fadeToggle(300);
+  $('.burger-btn').toggleClass('cross');
+  $('body').toggleClass('noscroll');
+});
+
+// スムーズスクロール
+$('a[href^="#"]').click(function(){
+  var adjust = 0;
+  var speed = 400;
+  var href= $(this).attr("href");
+  var target = $(href == "#" || href == "" ? 'html' : href);
+  var position = target.offset().top + adjust;
+  $('.header-nav').fadeToggle(300);
+  $('.burger-btn').toggleClass('cross');
+  $('body').toggleClass('noscroll');
+  $('body,html').animate({scrollTop:position}, speed, 'swing');
+  return false;
+});
